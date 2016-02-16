d2b74caa8971ac8cc259618ee6b26b8e6457ac78
Abdelkhalek Oussama Elhamer <abdelkhalekdev@gmail.com>
2016-02-16 14:05:44 +0100

    implement the Grid matrix

--- PacManGostSimulator/GostSimulator.cs
+++ PacManGostSimulator/GostSimulator.cs
@@ -4,78 +4,74 @@ using Microsoft.Xna.Framework.Input;
 
 namespace PacManGostSimulator
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+
     public class GostSimulator : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        private Grid _mainGrid;
 
         public GostSimulator()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
+        {            
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+        {            
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+        public void SetupGrid()
+        {
+            var gridMatrix = new int[,]
+                        {
+                            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                            {1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+                            {1,0,1,1,1,0,1,1,1,1,0,1,0,1,1,1,1,0,1,1,1,0,1},
+                            {1,0,1,1,1,0,1,1,1,1,0,1,0,1,1,1,1,0,1,1,1,0,1},
+                            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                            {1,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1},
+                            {1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1},
+                            {1,1,1,1,1,0,1,1,1,1,0,1,0,1,1,1,1,0,1,1,1,1,1},
+                            {1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1},
+                            {1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1},
+                            {1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+                            {1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1},
+                            {1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1},
+                            {1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1},
+                            {1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
+                            {1,1,0,1,1,0,1,1,1,1,0,1,0,1,1,1,1,0,1,1,0,1,1},
+                            {1,0,0,1,1,0,1,0,0,0,0,1,0,0,0,0,1,0,1,1,0,0,1},
+                            {1,1,0,0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,1,1},
+                            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                        };
+            
+
+            _mainGrid =new Grid(gridMatrix,Content.Load<Texture2D>("BlackRect"),Content.Load<Texture2D>("BlueRect"));
+        }
+
         protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
+        {            
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
+            
 
             base.Update(gameTime);
         }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.CornflowerBlue);            
 
             base.Draw(gameTime);
         }
---
+++ PacManGostSimulator/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PacManGostSimulator
+{
+    public class Grid
+    {
+        public static int Wall = 1;  //later add consder teh corners and the straight line walls 
+        public static int Space = 0;
+
+        public int[,] GridMap { get; set; }
+        public Texture2D BlackRect { get; set; }
+        public Texture2D BlueRect { get; set; } 
+        public Grid(int[,] gridMap,Texture2D blackRect,Texture2D blueRect)
+        {
+            GridMap = gridMap;
+            BlackRect = blackRect;
+            BlueRect = blueRect;
+        }
+         
+        public void DrawMap(SpriteBatch spriteBatch)    
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.End();            
+        }
+    }
+}
